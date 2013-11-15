01ce6a91c8b6f34ae46a2dcdc467fa3c7523a259
Tyler B <takvor@gmail.com>
2013-11-15 01:00:58 -0500

    Inititalize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Food::Application.config.secret_token = '5395244c157ebb62ae5fffb5936f1ff282cbb133c38882436ea14f1413b60fe45370c8f1611f8eebf85b1604cc9bfe64c63a34cfa8b4c1239b84c7ac43d480ba'
